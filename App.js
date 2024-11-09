d6b9bf316f180f67b0d9b8503fca2c42af1a0c4c
Ryan <ryanshaunsmith@gmail.com>
2024-11-09 17:45:15 +0000

    add input field and dynamic text

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, TextInput } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [name, setName] = useState("");
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hello World</Text>
       <Text>This is my first React Native App</Text>
+      <Text>{name ? `Hi ${name}` : "What is your name?"}</Text>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={(text) => setName(text)}
+      />
+      <Image />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,12 +23,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "grey",
+    backgroundColor: "#f5f5f5",
     alignItems: "center",
     justifyContent: "center",
+    gap: 10,
+    fontSize: 20,
   },
   title: {
-    fontSize: 20,
+    fontSize: 30,
     fontWeight: "bold",
+  },
+  textInput: {
+    backgroundColor: "#000",
+    width: "50%",
+    padding: 8,
+    color: "#fff",
+    textAlign: "center",
+    borderRadius: 10,
   },
 });
